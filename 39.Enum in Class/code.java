9c411ef7b0e0f3f3f4a7df12b28b135151e8d765
Arpan Chakraborty <chakrabortyarpan1212@gmail.com>
2025-07-15 00:58:05 +0530

    update

---
+++ 39.Enum in Class/code.java
@@ -0,0 +1,40 @@
+enum Laptop{
+    macbook(2000),thinkpad(1500),dell(),yoga(1800);
+
+    private int price;
+
+    private Laptop(int price) {
+        this.price = price;
+        System.out.println("In laptop "+this.name());
+
+    }
+
+ 
+
+    private Laptop() {
+        price =500;
+    }
+
+
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+    
+
+    
+
+
+}
+
+public class code {
+    public static void main(String[] args) {
+        for(Laptop lap: Laptop.values()){
+                System.out.println(lap+ " : "+lap.getPrice());
+        }
+    }
+}
